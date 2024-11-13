2a7c825e39427d67d523957557946760956db51c
Dan Balarin <dan.balarin@gmail.com>
2024-11-13 22:30:54 +0100

    chore(module-technology): fix lint errors

--- application/packages/module-technology/src/types/wappalyzer.d.ts
+++ application/packages/module-technology/src/types/wappalyzer.d.ts
@@ -1,43 +1,43 @@
 declare module "wappalyzer" {
-  type Headers = object;
+	type Headers = object;
 
-  type WappalyzerOptions = {
-    debug?: boolean;
-    delay?: number;
-    headers?: Headers;
-    maxDepth?: number;
-    maxUrls?: number;
-    maxWait?: number;
-    recursive?: boolean;
-    probe?: boolean;
-    proxy?: boolean;
-    userAgent?: string;
-    htmlMaxCols?: number;
-    htmlMaxRows?: number;
-    noScripts?: boolean;
-    noRedirect?: boolean;
-  };
+	type WappalyzerOptions = {
+		debug?: boolean;
+		delay?: number;
+		headers?: Headers;
+		maxDepth?: number;
+		maxUrls?: number;
+		maxWait?: number;
+		recursive?: boolean;
+		probe?: boolean;
+		proxy?: boolean;
+		userAgent?: string;
+		htmlMaxCols?: number;
+		htmlMaxRows?: number;
+		noScripts?: boolean;
+		noRedirect?: boolean;
+	};
 
-  type Storage = {
-    local: object;
-    session: object;
-  };
+	type Storage = {
+		local: object;
+		session: object;
+	};
 
-  class Wappalyzer {
-    constructor(options: WappalyzerOptions);
+	class Wappalyzer {
+		constructor(options: WappalyzerOptions);
 
-    public async init(): Promise<void>;
+		public init(): Promise<void>;
 
-    public async open(
-      url: string,
-      headers?: Headers,
-      storage?: Storage,
-    ): Promise<void>;
+		public open(
+			url: string,
+			headers?: Headers,
+			storage?: Storage,
+		): Promise<void>;
 
-    public async analyze(): Promise<unknown>;
+		public analyze(): Promise<unknown>;
 
-    public async destroy(): Promise<void>;
-  }
+		public destroy(): Promise<void>;
+	}
 
-  export default Wappalyzer;
+	export default Wappalyzer;
 }
