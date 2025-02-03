323b062b35f31594d3775fdf22f17c32f8bb1c0c
Dan Balarin <dan.balarin@gmail.com>
2025-02-04 00:27:08 +0100

    feat: add knowledge base routes

---
+++ application/apps/web/src/app/(main-navigation)/knowledge-base/layout.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+import { KnowledgeBaseLayout } from "~/features/knowledge-base/components/KnowledgeBaseLayout";
+
+export const metadata: Metadata = {
+	title: "Knowledge Base",
+	description: "Knowledge Base",
+};
+
+export default KnowledgeBaseLayout;
