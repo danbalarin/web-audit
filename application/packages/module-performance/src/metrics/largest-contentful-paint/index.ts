e91717cee2d256d2746ed0e052fadbcc48264190
Dan Balarin <dan.balarin@gmail.com>
2025-02-03 02:35:39 +0100

    feat: add compare and rank to metrics

--- application/packages/module-performance/src/metrics/largest-contentful-paint/index.ts
+++ application/packages/module-performance/src/metrics/largest-contentful-paint/index.ts
@@ -1,12 +1,27 @@
 "use client";
-import { AuditMetricDescription } from "@repo/api";
+import {
+	AuditMetricDescription,
+	createMetricCompareLowerIsBetter,
+} from "@repo/api";
 import description from "./description.mdx";
 
-export const LargestContentfulPaint: AuditMetricDescription = {
+export const LargestContentfulPaint: AuditMetricDescription<number> = {
 	id: "largest-contentful-paint",
 	name: "Largest Contentful Paint",
 	shortDescription:
 		"The Largest Contentful Paint (LCP) metric reports the render time of the largest content element visible within the viewport.",
 	fullDescription: description,
 	unit: "ms",
+	compare: createMetricCompareLowerIsBetter(100),
+	rank: (value) => {
+		if (value <= 2500) {
+			return "good";
+		}
+
+		if (value <= 4000) {
+			return "average";
+		}
+
+		return "fail";
+	},
 };
