8d0bb00180cf4afd36f075671c59f2100a263496
Dan Balarin <dan.balarin@gmail.com>
2025-02-05 23:12:33 +0100

    fix: ssr sidebar

--- application/apps/web/src/app/(main-navigation)/layout.tsx
+++ application/apps/web/src/app/(main-navigation)/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
 import { SidebarLayout } from "~/features/report/components/SidebarLayout";
+import { HydrateClient, trpc } from "~/server/query/server";
 
 export const metadata: Metadata = {
 	title: "Audit projects",
 };
 
-export default SidebarLayout;
+export default ({ children }: { children: React.ReactNode }) => {
+	void trpc.projects.findAll.prefetch();
+
+	return (
+		<HydrateClient>
+			<SidebarLayout>{children}</SidebarLayout>
+		</HydrateClient>
+	);
+};
