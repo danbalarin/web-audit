17327d0d3d719da7c3f223fc35b3624d90e23f9d
Dan Balarin <dan.balarin@gmail.com>
2025-02-05 22:57:07 +0100

    chore: electric sql deps cleanup

--- application/apps/web/src/features/report/pages/HomePage.tsx
+++ application/apps/web/src/features/report/pages/HomePage.tsx
@@ -1,13 +1,3 @@
-"use client";
-import { usePGlite } from "@electric-sql/pglite-react";
-import { Repl } from "@electric-sql/pglite-repl";
-
 export function HomePage() {
-	const pg = usePGlite();
-
-	return (
-		<div>
-			<Repl pg={pg} />
-		</div>
-	);
+	return <div>Home Page</div>;
 }
