73eb7e14082ab66241c66d119b11e24ddc83594e
Dan Balarin <dan.balarin@gmail.com>
2025-02-27 13:15:43 +0100

    feat: add score tooltip, no data metric cell

--- application/apps/web/src/features/report/components/MetricResultCell/MetricResultCell.tsx
+++ application/apps/web/src/features/report/components/MetricResultCell/MetricResultCell.tsx
@@ -1,7 +1,7 @@
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import DangerousIcon from "@mui/icons-material/Dangerous";
 import WarningIcon from "@mui/icons-material/Warning";
-import { Typography } from "@mui/material";
+import { Tooltip, Typography } from "@mui/material";
 
 import { Memory, Time } from "@repo/api/metrics";
 import { CalculatedScore, MetricDescription } from "@repo/api/types";
@@ -19,7 +19,14 @@ export const MetricResultCell = ({
 	description,
 }: MetricResultCellProps) => {
 	if (!data) {
-		return null;
+		return (
+			<Tooltip
+				title="No data were retrieved for this metric"
+				placement="bottom-start"
+			>
+				<Typography>Not scored</Typography>
+			</Tooltip>
+		);
 	}
 	let component;
 	switch (description.unit) {
@@ -45,24 +52,26 @@ export const MetricResultCell = ({
 				? "error"
 				: "warning";
 	return (
-		<Typography
-			component={"span"}
-			variant="body1"
-			sx={{
-				display: "flex",
-				alignItems: "center",
-				gap: 1,
-			}}
-		>
-			{data.rank === "good" ? (
-				<CheckCircleIcon fontSize="small" color={color} />
-			) : data.rank === "fail" ? (
-				<DangerousIcon fontSize="small" color={color} />
-			) : (
-				<WarningIcon fontSize="small" color={color} />
-			)}
-			{component}
-		</Typography>
+		<Tooltip title={Math.round(data.score) + "%"} placement="bottom-start">
+			<Typography
+				component={"span"}
+				variant="body1"
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					gap: 1,
+				}}
+			>
+				{data.rank === "good" ? (
+					<CheckCircleIcon fontSize="small" color={color} />
+				) : data.rank === "fail" ? (
+					<DangerousIcon fontSize="small" color={color} />
+				) : (
+					<WarningIcon fontSize="small" color={color} />
+				)}
+				{component}
+			</Typography>
+		</Tooltip>
 	);
 };
 
