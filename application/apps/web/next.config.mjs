e273f7075bf30af766c53f700a57c991c6618648
Dan Balarin <dan.balarin@gmail.com>
2025-03-19 14:58:57 +0100

    fix: mdn build requirements

--- application/apps/web/next.config.mjs
+++ application/apps/web/next.config.mjs
@@ -1,30 +1,56 @@
 import createBundleAnalyzer from "@next/bundle-analyzer";
 import createMDX from "@next/mdx";
 import { withPigment } from "@pigment-css/nextjs-plugin";
+import { nodeFileTrace } from "@vercel/nft";
 
+import path from "path";
+import { fileURLToPath } from "url";
 import "./src/env.mjs";
 import { theme } from "./src/features/ui/components/ThemeRegistry/theme.mjs";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-	output: "standalone",
-	serverExternalPackages: ["lighthouse", "lighthouse/cli"],
-	experimental: {
-		mdxRs: true,
-	},
-	redirects: () => [
-		{
-			source: "/",
-			destination: "/project",
-			permanent: false,
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const nextConfig = async () => {
+	const mdnFiles = [
+		(await import.meta.resolve("@mdn/mdn-http-observatory"))
+			.replace("file://", "")
+			.replace("src/index.js", "conf/hsts-preload.json"),
+	];
+	const { fileList: additionalTracedFiles } = await nodeFileTrace(mdnFiles, {
+		// ignore unnecesary files if nft includes too many
+		ignore: (file) => {
+			return file.replace(/\\/g, "/").startsWith("node_modules/.bin/");
 		},
-		{
-			source: "/project",
-			destination: "/project/new",
-			permanent: true,
+	});
+
+	/** @type {import('next').NextConfig} */
+	return {
+		pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+		output: "standalone",
+		serverExternalPackages: ["lighthouse", "lighthouse/cli"],
+		experimental: {
+			mdxRs: true,
 		},
-	],
+		// outputFileTracingRoot: path.resolve(
+		// 	path.dirname(fileURLToPath(import.meta.url)),
+		// 	"../../",
+		// ),
+		outputFileTracingIncludes: {
+			"**": [...additionalTracedFiles],
+		},
+		redirects: () => [
+			{
+				source: "/",
+				destination: "/project",
+				permanent: false,
+			},
+			{
+				source: "/project",
+				destination: "/project/new",
+				permanent: true,
+			},
+		],
+	};
 };
 
 /** @type {import('@pigment-css/nextjs-plugin').PigmentOptions} */
@@ -46,5 +72,5 @@ const withBundleAnalyzer = createBundleAnalyzer({
 });
 
 export default withBundleAnalyzer(
-	withMDX(withPigment(nextConfig, pigmentConfig)),
+	withMDX(withPigment(await nextConfig(), pigmentConfig)),
 );
