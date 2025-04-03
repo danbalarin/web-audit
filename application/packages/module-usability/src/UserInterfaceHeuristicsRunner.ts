92c2e184e56d164275bb333605994bf98633cca3
Dan Balarin <dan.balarin@gmail.com>
2025-04-03 14:50:22 +0200

    feat: add heuristics metrics to the results

--- application/packages/module-usability/src/UserInterfaceHeuristicsRunner.ts
+++ application/packages/module-usability/src/UserInterfaceHeuristicsRunner.ts
@@ -6,8 +6,15 @@ import type {
 } from "@repo/api/types";
 
 import { EMPTY_INPUT } from "@repo/api/metrics";
+import { ActionConsistency } from "./metrics/action-consistency";
+import { Aesthetics } from "./metrics/aesthetics";
+import { BrandCompliance } from "./metrics/brand-compliance";
 import { ClearFeedback } from "./metrics/clear-feedback";
+import { ContextualCues } from "./metrics/contextual-cues";
+import { FlexibleNavigation } from "./metrics/flexible-navigation";
+import { IconMetaphors } from "./metrics/icon-metaphors";
 import { ImmediateFeedback } from "./metrics/immediate-feedback";
+import { VisualConsistency } from "./metrics/visual-consistency";
 
 export type UserInterfaceHeuristicsRunnerOptions = BaseRunnerOptions;
 
@@ -25,11 +32,39 @@ export class UserInterfaceHeuristicsRunner extends BaseRunner<
 	async transform(_res: Result): Promise<MetricResult[]> {
 		return [
 			{
-				id: ImmediateFeedback.id,
+				id: ActionConsistency.id,
+				value: EMPTY_INPUT,
+			},
+			{
+				id: Aesthetics.id,
+				value: EMPTY_INPUT,
+			},
+			{
+				id: BrandCompliance.id,
 				value: EMPTY_INPUT,
 			},
 			{
 				id: ClearFeedback.id,
+				value: EMPTY_INPUT,
+			},
+			{
+				id: ContextualCues.id,
+				value: EMPTY_INPUT,
+			},
+			{
+				id: FlexibleNavigation.id,
+				value: EMPTY_INPUT,
+			},
+			{
+				id: IconMetaphors.id,
+				value: EMPTY_INPUT,
+			},
+			{
+				id: ImmediateFeedback.id,
+				value: EMPTY_INPUT,
+			},
+			{
+				id: VisualConsistency.id,
 				value: EMPTY_INPUT,
 			},
 		];
